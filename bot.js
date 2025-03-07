e9c8a54bb9a4a0c263c29758a76d71a7cb897cc4
spxrtxk <spartakvasilev1999@gmail.com>
2025-03-07 14:30:05 +0900

    chore: clean code

--- bot.js
+++ bot.js
@@ -1,13 +1,12 @@
 require('dotenv').config();
 const { Telegraf, Scenes, session } = require('telegraf');
 const scenes = require('./scenes');
-const { mainKeyboard } = require('./helpers/keyboards');
-const { START_MESSAGE, FUNCTION_IN_DEVELOPMENT } = require('./config/constants');
+const { START_MESSAGE } = require('./config/constants');
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
 const bot = new Telegraf(token);
-
 const stage = new Scenes.Stage(scenes);
+
 bot.use(session());
 bot.use(stage.middleware());
 
@@ -24,18 +23,7 @@ bot.command('start', ctx => {
     });
 });
 
-// bot.command('start', ctx => 
-//     ctx.reply(START_MESSAGE(ctx.from.first_name), mainKeyboard)
-// );
-
-// bot.hears('Часто задаваемые вопросы ⁉️', ctx => ctx.scene.enter('FAQ_SCENE'));
-// bot.hears('Задать вопрос 📝', ctx => ctx.scene.enter('REQUEST_SCENE'));
-// bot.hears('Открыть доступ к приложению 📱', ctx => ctx.scene.enter('UNLOCK_APP_SCENE'));
-// bot.hears('Открыть доступ к марафону 🏆', ctx => ctx.scene.enter('UNLOCK_MARATHON_SCENE'));
-// bot.hears('Открыть доступ к марафону 🏆', ctx => ctx.reply(FUNCTION_IN_DEVELOPMENT, mainKeyboard));
-
 bot.launch();
-console.log('Бот запущен!');
 
 process.on('unhandledRejection', error => {
     console.error('Unhandled Rejection:', error);
